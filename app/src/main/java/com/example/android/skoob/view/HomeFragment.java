ea0d75d5c06426fde712d961933cbefafe41bea1
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-07-09 01:07:36 -0600

    Implement Google Play Services to get user location
    I return the city name the user is on.
    
    I will be using Fused Location API that combines signals from GPS, Wi-Fi, and cell networks, as well as an accelerometer, gyroscope, magnetometer and other sensors to provide more accurate results.

--- app/src/main/java/com/example/android/skoob/view/HomeFragment.java
+++ app/src/main/java/com/example/android/skoob/view/HomeFragment.java
@@ -1,14 +1,31 @@
 package com.example.android.skoob.view;
 
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.skoob.R;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
+import java.util.Locale;
 
 
 /**
@@ -17,6 +34,18 @@ import com.example.android.skoob.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+    // location updates interval - 10sec
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    // fastest updates interval - 5 sec
+    // location updates will be received if another app is requesting the locations
+    // than your app can handle
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
+
+    private ProgressBar mProgressBar;
+    private TextView mTextLongLat;
+    private TextView mTextCity;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -31,7 +60,101 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mProgressBar = rootView.findViewById(R.id.progressBar);
+        mTextLongLat = rootView.findViewById(R.id.textLongLat);
+        mTextCity = rootView.findViewById(R.id.tv_city);
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            AddLocationPermission();
+            getCurrentLocation();
+        }else{
+            getCurrentLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == PERMISSIONS_REQUEST_FINE_LOCATION && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }else{
+                Toast.makeText(getContext(), getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void AddLocationPermission() {
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_FINE_LOCATION);
+    }
+
+    private void getCurrentLocation(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        final LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.getFusedLocationProviderClient(getActivity())
+                .requestLocationUpdates(locationRequest, new LocationCallback(){
+
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(getActivity())
+                                .removeLocationUpdates(this);
+                        if(locationResult != null & locationResult.getLocations().size() > 0){
+                            int latestLocationIndex = locationResult.getLocations().size() - 1;
+                            double latitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                            double longitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                            mTextLongLat.setText(String.format(
+                                    "Latitude: %s\nLongitude: %s",
+                                    latitude,
+                                    longitude
+                            ));
+
+                            Location location = new Location("providerNA");
+                            location.setLatitude(latitude);
+                            location.setLongitude(longitude);
+                            fetchCityFromLatLong(location);
+                        }else {
+                            mProgressBar.setVisibility(View.GONE);
+                        }
+                    }
+                }, Looper.getMainLooper());
+    }
+
+    private void fetchCityFromLatLong(Location location){
+
+        Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
+        List<Address> addresses;
+        String cityName;
+        try{
+            addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            cityName = addresses.get(0).getLocality(); //get the city
+        }catch(Exception exception){
+            cityName = exception.getMessage(); //assign error message to display
+        }
+
+        mTextCity.setText(cityName);
+        mProgressBar.setVisibility(View.GONE);
+
     }
 
 }
