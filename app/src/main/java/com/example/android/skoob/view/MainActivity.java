4382bb64a6f3061a40e6fa200002f24ccb4b0c25
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-08-03 01:07:23 -0600

    Corrected som code review given by Udacity
    Hard coded texts
    images with no image description
    Handled my permission call better, when the permission call is rejected by the user. I just load the list like that without location. However, when the load back the app onCreate i will request the location permission again. Have a look at MainActivity

--- app/src/main/java/com/example/android/skoob/view/MainActivity.java
+++ app/src/main/java/com/example/android/skoob/view/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -17,6 +18,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -24,10 +26,17 @@ import com.example.android.skoob.R;
 import com.example.android.skoob.model.Book;
 import com.example.android.skoob.repository.BookRepository;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -74,7 +83,6 @@ public class MainActivity extends AppCompatActivity implements SettingsFragment.
     private Handler statusUpdateHandler = new Handler();
     private Runnable statusUpdateRunnable;
 
-    //private FavouritesViewModel mFavouritesViewModel;
     private BookRepository mBookRepository;
 
     @Override
@@ -220,36 +228,52 @@ public class MainActivity extends AppCompatActivity implements SettingsFragment.
         locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        LocationServices.getFusedLocationProviderClient(this)
-                .requestLocationUpdates(locationRequest, new LocationCallback(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            AddListenersForFirebase();
+            return;
+        }
 
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        super.onLocationResult(locationResult);
-                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                                .removeLocationUpdates(this);
-                        if(locationResult != null & locationResult.getLocations().size() > 0){
-                            int latestLocationIndex = locationResult.getLocations().size() - 1;
-                            double latitude =
-                                    locationResult.getLocations().get(latestLocationIndex).getLatitude();
-                            double longitude =
-                                    locationResult.getLocations().get(latestLocationIndex).getLongitude();
-                            /*mTextLongLat.setText(String.format(
-                                    "Latitude: %s\nLongitude: %s",
-                                    latitude,
-                                    longitude
-                            ));*/
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        Task<LocationSettingsResponse> result =
+                LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
 
-                            Location location = new Location("providerNA");
-                            location.setLatitude(latitude);
-                            location.setLongitude(longitude);
-                            fetchCityFromLatLong(location);
-                            //When we are done getting the current user location, we now initialize event listener
-                            //for firebase to get the list of books for sale from database
-                            AddListenersForFirebase();
-                        }
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    // All location settings are satisfied. The client can initialize location
+                    // requests here.
+                    getFusedLocation(locationRequest);
+                } catch (ApiException exception) {
+                    switch (exception.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be fixed by showing the
+                            // user a dialog.
+                            try {
+                                // Cast to a resolvable exception.
+                                ResolvableApiException resolvable = (ResolvableApiException) exception;
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                resolvable.startResolutionForResult(
+                                        MainActivity.this,
+                                        LocationRequest.PRIORITY_HIGH_ACCURACY);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            } catch (ClassCastException e) {
+                                // Ignore, should be an impossible error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way to fix the
+                            // settings so we won't show the dialog.
+                            break;
                     }
-                }, Looper.getMainLooper());
+                }
+            }
+        });
     }
 
     private void fetchCityFromLatLong(Location location){
@@ -264,6 +288,34 @@ public class MainActivity extends AppCompatActivity implements SettingsFragment.
             cityName = exception.getMessage(); //assign error message to display
         }
 
+    }
+
+    private void getFusedLocation(LocationRequest locationRequest){
+        LocationServices.getFusedLocationProviderClient(this)
+                .requestLocationUpdates(locationRequest, new LocationCallback(){
+
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                .removeLocationUpdates(this);
+                        if(locationResult != null & locationResult.getLocations().size() > 0){
+                            int latestLocationIndex = locationResult.getLocations().size() - 1;
+                            double latitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                            double longitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLongitude();
+
+                            Location location = new Location("providerNA");
+                            location.setLatitude(latitude);
+                            location.setLongitude(longitude);
+                            fetchCityFromLatLong(location);
+                            //When we are done getting the current user location, we now initialize event listener
+                            //for firebase to get the list of books for sale from database
+                            AddListenersForFirebase();
+                        }
+                    }
+                }, Looper.getMainLooper());
     }
 
     @Override
@@ -296,6 +348,14 @@ public class MainActivity extends AppCompatActivity implements SettingsFragment.
                     transaction.replace(R.id.main_frame, fragmentToLoad);
                     transaction.commit();
                 }
+            }
+        }else if(requestCode == LocationRequest.PRIORITY_HIGH_ACCURACY){
+            if (resultCode == RESULT_OK) {
+                // All required changes were successfully made
+                Log.i("MainActivity", "onActivityResult: GPS Enabled by user");
+                getCurrentLocation();
+            }else if(resultCode == RESULT_CANCELED){
+                AddListenersForFirebase();
             }
         }else if (resultCode == RESULT_CANCELED) {
             // Sign in was canceled by the user, finish the activity
