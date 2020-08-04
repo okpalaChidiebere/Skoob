245bc4a31a6f79a0db2429051c0a2835a49a67e9
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-08-03 23:58:55 -0600

    used the gradle.properties file to save this information as a System env Eg encryption keys
    By default, you will put the gradle.properties file in git ignore

--- app/src/main/java/com/example/android/skoob/utils/Constants.java
+++ app/src/main/java/com/example/android/skoob/utils/Constants.java
@@ -1,12 +1,14 @@
 package com.example.android.skoob.utils;
 
+import com.example.android.skoob.BuildConfig;
+
 public class Constants {
 
-    public static final String GEO_API_KEY = "AIzaSyCir2Pcz-LENT64X0NwORNJ7R1gaX2QhOg";
+    public static final String GEO_API_KEY = BuildConfig.GEO_API_KEY;
     public static final String SKOOB_SHARE_HASHTAG = " #SKOOB";
     public static final String EXTRA_BOOK_DETAILS = "EXTRA_BOOK_DETAILS";
     public static final String EXTRA_USER_LOGIN_EMAIL = "EXTRA_USER_LOGIN_EMAIL";
-    public static final String SECRET_AES_KEY = "ssshhhhhhhhhhh!!!!";
+    public static final String SECRET_AES_KEY = BuildConfig.SECRET_AES_KEY;
     public static final String BUNDLE_BOOKS_FOR_SALE = "BUNDLE_BOOKS_FOR_SALE";
     public static final String EXTRA_BOOKS_FOR_SALE = "EXTRA_BOOKS_FOR_SALE";
     public static final String ACTION_WIDGET_OPEN_BOOK_DETAIL = "ACTION_WIDGET_OPEN_BOOK_DETAIL";
